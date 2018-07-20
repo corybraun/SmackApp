cfe179634594c86b71e37e99a15134967b8e8c6d
Cory Braun <corybraun@Corys-MacBook-Pro.local>
2018-07-19 22:10:16 -0700

    Created the UI for the login screen. Lesosn-04

---
+++ SmackApp/SmackApp/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  SmackApp
+//
+//  Created by Cory Braun on 7/19/18.
+//  Copyright © 2018 Cory Braun. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
---
+++ SmackApp/SmackApp/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  SmackApp
+//
+//  Created by Cory Braun on 7/19/18.
+//  Copyright © 2018 Cory Braun. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+
+let TO_LOGIN = "toLogin"
