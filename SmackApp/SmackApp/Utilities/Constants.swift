7903a12e79f5a182922e030d7eb3aeb7e83d39c3
Cory Braun <corybraun@Corys-MacBook-Pro.local>
2018-07-22 23:30:39 -0700

    Added IBActions and IBOutlets for CreateAccountVC

--- SmackApp/SmackApp/Utilities/Constants.swift
+++ SmackApp/SmackApp/Utilities/Constants.swift
@@ -17,7 +17,7 @@ import Foundation
 typealias CompletionHandler =  (_ Success: Bool) -> ()
 
 //URL Constants
-let BASE_URL = " https://smackchatappchat.herokuapp.com/v1/"
+let BASE_URL = "https://smackchatappchat.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 
 
