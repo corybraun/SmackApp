7e6d45a8f53e80432a2803ddbcdd427983b7d8e0
Cory Braun <corybraun@Corys-MacBook-Pro.local>
2018-07-22 22:28:27 -0700

    Created AuthService. Created ability to Register a user.

--- SmackApp/SmackApp/Utilities/Constants.swift
+++ SmackApp/SmackApp/Utilities/Constants.swift
@@ -8,8 +8,27 @@
 
 import Foundation
 
+
+// typealias is a way to remap a type. So if I did:
+// typealias Jonny = String
+// let name: Jonny = "Jonny"
+// it's name Jonny a type with the same exact characteristics as a String.
+
+typealias CompletionHandler =  (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = " https://smackchatappchat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
