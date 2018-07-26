a8f2abc3851db33dc1a25c5e46eb4023f14a6e9c
Cory Braun <corybraun@Corys-MacBook-Pro.local>
2018-07-25 18:05:31 -0700

    Created the Avatar UI

---
+++ SmackApp/SmackApp/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  SmackApp
+//
+//  Created by Cory Braun on 7/25/18.
+//  Copyright © 2018 Cory Braun. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}
